1fa502848b46685832accb1b85954961805b0395
Kush Patel <w0859776@myscc.ca>
2025-03-20 20:07:26 -0400

    Implement explicit intent to display expense details

--- app/src/main/java/com/example/kushpatel_0859776_androidassignments/ExpenseDetailsActivity.kt
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/ExpenseDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kushpatel_0859776_androidassignments
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kushpatel_0859776_androidassignment6.R
 
@@ -8,5 +10,22 @@ class ExpenseDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense_details)
+
+        val tvName = findViewById<TextView>(R.id.textViewExpenseName)
+        val tvAmount = findViewById<TextView>(R.id.textViewExpenseAmount)
+        val tvDate = findViewById<TextView>(R.id.textViewExpenseDate)
+        val btnBackHome = findViewById<Button>(R.id.buttonBackHome)
+
+        val name = intent.getStringExtra("expense_name")
+        val amount = intent.getStringExtra("expense_amount")
+        val date = intent.getStringExtra("expense_date")
+
+        tvName.text = "Name: $name"
+        tvAmount.text = "Amount: $amount"
+        tvDate.text = "Date: $date"
+
+        buttonBackHome.setOnClickListener {
+            finish()
+        }
     }
 }
