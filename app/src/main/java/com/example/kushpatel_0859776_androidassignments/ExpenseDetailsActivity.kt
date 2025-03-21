0ad557420025b0ac663f20fe8e5b68f7ea757082
Kush Patel <w0859776@myscc.ca>
2025-03-20 20:02:11 -0400

    Created ExpenseDetailsActivity file

---
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/ExpenseDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kushpatel_0859776_androidassignments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kushpatel_0859776_androidassignment6.R
+
+class ExpenseDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_details)
+    }
+}
