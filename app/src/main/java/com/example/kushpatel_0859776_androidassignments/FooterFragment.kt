7a8862fc7906e0ce16af9701e852c44421ce7088
Kush Patel <w0859776@myscc.ca>
2025-03-20 23:52:10 -0400

    Implemented FooterFragment to display total expenses and update it dynamically.

--- app/src/main/java/com/example/kushpatel_0859776_androidassignments/FooterFragment.kt
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/FooterFragment.kt
@@ -1,2 +1,31 @@
 package com.example.kushpatel_0859776_androidassignments
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.kushpatel_0859776_androidassignment6.R
+
+class FooterFragment : Fragment() {
+    private var totalExpense = 0.0
+    private lateinit var textViewTotalExpense: TextView
+
+    @SuppressLint("SetTextI18n")
+    fun updateTotalExpense(amount: Double) {
+        totalExpense += amount
+        textViewTotalExpense.text = "Total Expense: $$totalExpense"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_footer, container, false)
+        textViewTotalExpense = view.findViewById(R.id.textViewTotalExpense)
+        return view
+    }
+}
