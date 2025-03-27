fe7884614e9861d0fffbe18b705ce98f73d70002
Kush Patel <w0859776@myscc.ca>
2025-03-27 11:49:04 -0400

    Override the function onCreateView

---
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/ExpenseDetailsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.kushpatel_0859776_androidassignments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kushpatel_0859776_androidassignment6.R
+
+
+class ExpenseDetailsFragment : Fragment() {
+    override fun onCreateView (
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_expense_details, container, false)
+    }
+}
