a1bfcd88939d212934cf786a399aba12ad73c688
Kush Patel <w0859776@myscc.ca>
2025-04-02 01:49:39 -0400

    Created Retrofit Instance using BASE URL

--- app/src/main/java/com/example/kushpatel_0859776_androidassignments/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/network/RetrofitInstance.kt
@@ -1,4 +1,16 @@
 package com.example.kushpatel_0859776_androidassignments.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+    private const val BASE_URL = "https://api.exchangerate-api.com/v4/latest/"
+
+    val api: CurrencyApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CurrencyApiService::class.java)
+    }
 }
