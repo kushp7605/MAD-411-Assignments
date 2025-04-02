c6e1345d5c1ff893bfd95a2d01eded223a900bf2
Kush Patel <w0859776@myscc.ca>
2025-04-02 14:52:48 -0400

    Implemented CurrencyApiService interface for API calls

--- app/src/main/java/com/example/kushpatel_0859776_androidassignments/network/CurrencyApiService.kt
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/network/CurrencyApiService.kt
@@ -3,8 +3,6 @@ package com.example.kushpatel_0859776_androidassignments.network
 import com.example.kushpatel_0859776_androidassignments.models.ExchangeRatesResponse
 import retrofit2.http.GET
 
-
-
 interface CurrencyApiService {
     @GET("currencies.json")
     suspend fun getCurrencies(): Map<String, String>
