cb866b9b66e35e1f7552251fca93c39774549e1c
Kush Patel <w0859776@myscc.ca>
2025-04-02 23:59:26 -0400

    Moved the file to the activities folder

--- app/src/main/java/com/example/kushpatel_0859776_androidassignments/activities/MainActivity.kt
+++ app/src/main/java/com/example/kushpatel_0859776_androidassignments/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.kushpatel_0859776_androidassignments
+package com.example.kushpatel_0859776_androidassignments.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
